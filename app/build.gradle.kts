5b8a5222647e35506ef56ed763f34d0b1e55153c
Roman Mityukov <r.v.mityukov@yandex.ru>
2025-07-11 16:14:13 +0700

    bump v0.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "io.mityukov.geo.tracking"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.0"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 

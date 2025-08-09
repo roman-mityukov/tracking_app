006908be5dfffe133eb4c5e56c74e434f4789aaa
Roman Mityukov <r.v.mityukov@yandex.ru>
2025-08-10 06:20:24 +0700

    Use @Transaction for track deletion

--- src/app/src/main/java/io/mityukov/geo/tracking/core/database/dao/TrackDao.kt
+++ src/app/src/main/java/io/mityukov/geo/tracking/core/database/dao/TrackDao.kt
@@ -1,10 +1,12 @@
 @file:Suppress("TooManyFunctions")
+
 package io.mityukov.geo.tracking.core.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import io.mityukov.geo.tracking.core.database.model.TrackActionEntity
 import io.mityukov.geo.tracking.core.database.model.TrackEntity
 import io.mityukov.geo.tracking.core.database.model.TrackPointEntity
@@ -16,12 +18,14 @@ interface TrackDao {
     @Query("SELECT * FROM track")
     fun getAllTracks(): Flow<List<TrackEntity>>
 
+    @Transaction
     @Query("SELECT * FROM track")
     fun getAllTracksWithPoints(): Flow<List<TrackWithPoints>>
 
     @Query("SELECT * FROM track WHERE id=:id")
     fun getTrack(id: String): TrackEntity
 
+    @Transaction
     @Query("SELECT * FROM track WHERE id=:id")
     fun getTrackWithPoints(id: String): Flow<TrackWithPoints>
 
@@ -45,4 +49,11 @@ interface TrackDao {
 
     @Query("DELETE FROM track_action WHERE track_id=:trackId")
     fun deleteTrackActions(trackId: String)
+
+    @Transaction
+    fun deleteAllTrackData(trackId: String) {
+        deleteTrackPoints(trackId)
+        deleteTrackActions(trackId)
+        deleteTrack(trackId)
+    }
 }

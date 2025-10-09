b4890bf6de4377d8db46614fe0f285739a1a2720
Roman Mityukov <r.v.mityukov@yandex.ru>
2025-10-09 09:27:01 +0700

    Delete empty test

--- src/core/data/src/test/java/io/mityukov/geo/tracking/core/data/repository/track/TrackRepositoryTest.kt
+++
@@ -1,9 +0,0 @@
-package io.mityukov.geo.tracking.core.data.repository.track
-
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-
-@Suppress("EmptyClassBlock")
-@RunWith(RobolectricTestRunner::class)
-class TrackRepositoryTest {
-}

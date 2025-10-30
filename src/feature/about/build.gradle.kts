45ec7fcc6e2f953f435ba9c22d8b5aad75a60e21
Roman Mityukov <r.v.mityukov@yandex.ru>
2025-10-30 15:40:25 +0700

    Add lottie animation to About screen

--- src/feature/about/build.gradle.kts
+++ src/feature/about/build.gradle.kts
@@ -14,4 +14,5 @@ dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:sharing"))
     implementation(libs.androidx.material3)
+    implementation(libs.lottie)
 }
